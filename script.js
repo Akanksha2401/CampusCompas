b5f717c48c445214b42319f148d569df55c59cef
Akanksha2401 <56122932+Akanksha2401@users.noreply.github.com>
2024-03-03 09:49:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('loginBtn').addEventListener('click', function() {
+  alert('Please log in to access your account.');
+});
+
+document.getElementById('signupBtn').addEventListener('click', function() {
+  alert('Sign up to create a new account.');
+});
